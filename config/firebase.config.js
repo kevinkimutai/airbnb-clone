16e6e4529239e7ad32a16348e4a9d7e79bd45c00
unknown <kevinkimutai62@gmail.com>
2023-09-02 00:39:31 +0300

    firebase config

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtrvakDy_X9Szi5rvu9D76FuStR1x1qlo",
-  authDomain: "airbnb-clone-397319.firebaseapp.com",
-  projectId: "airbnb-clone-397319",
-  storageBucket: "airbnb-clone-397319.appspot.com",
-  messagingSenderId: "130001966602",
-  appId: "1:130001966602:web:842232692a315a1e13b1f3",
-  measurementId: "G-3L6DNSEFQH",
+  apiKey: process.env.FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
